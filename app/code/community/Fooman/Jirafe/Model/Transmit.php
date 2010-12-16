8bd14ba0271b5a95cb1a9b11e39020de09f4530a
kristof@fooman.co.nz <none@none>
2010-12-16 18:06:59 +1300

    add boilerplate

--- app/code/community/Fooman/Jirafe/Model/Transmit.php
+++ app/code/community/Fooman/Jirafe/Model/Transmit.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @package     Fooman_Jirafe
+ * @copyright   Copyright (c) 2010 Jirafe Inc (http://www.jirafe.com)
+ * @copyright   Copyright (c) 2010 Fooman Limited (http://www.fooman.co.nz)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 class Fooman_Jirafe_Model_Transmit extends Mage_Core_Model_Abstract
 {
     protected $_helper = '';
