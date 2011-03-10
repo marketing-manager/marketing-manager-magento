faeca8c27be12fa84c68bd134c44a1ffc7211482
kristof@fooman.co.nz <none@none>
2011-03-10 22:41:42 +1300

    call sync when deleting/saving user/store

--- app/code/community/Fooman/Jirafe/Model/Observer.php
+++ app/code/community/Fooman/Jirafe/Model/Observer.php
@@ -29,49 +29,101 @@ class Fooman_Jirafe_Model_Observer
     }
      */
 
+    /**
+     * sync a jirafe store when adding a new store or after saving
+     * an existing store
+     * checks local settings hash for settings before sync
+     *
+     * @param $observer
+     */
     public function syncJirafeStore ($observer)
     {
         $jirafe = Mage::getModel('foomanjirafe/jirafe');
         $appId = $jirafe->checkAppId();
         if ($appId) {
-            $store = $observer->getEvent()->getObject();
+            $store = $observer->getEvent()->getStore();
             $jirafe->checkSiteId($appId, $store);
         }
     }
 
+    /**
+     * sync all stores and users with Jirafe
+     * called after deleting a store
+     *
+     * @param $observer
+     */
+    public function fullSyncJirafeStore ($observer)
+    {
+        $jirafe = Mage::getModel('foomanjirafe/jirafe');
+        $appId = $jirafe->checkAppId();
+        if ($appId) {
+            $jirafe->syncUsersAndStores();
+        }
+    }
 
+    /**
+     * sync all stores and users with Jirafe
+     * called after saving a user
+     *
+     * @param $observer
+     */
+    public function fullSyncNewUser ($observer)
+    {
+        $jirafeEmailReportType = Mage::app()->getRequest()->getPost('jirafe_email_report_type');
+        if(!$jirafeEmailReportType) {
+            //we don't have Jirafe POST data from the My Account Form = we are adding a new user
+            //TODO: is also called when updating a user via System > Role
+            $jirafe = Mage::getModel('foomanjirafe/jirafe');
+            $appId = $jirafe->checkAppId();
+            if ($appId) {
+                $jirafe->syncUsersAndStores();
+            }
+        }
+    }    
+
+    /**
+     * sync all stores and users with Jirafe
+     * called after saving a user
+     * use POST data to identify update to existing users
+     * only call sync if Jirafe relevant data has changed
+     *
+     * @param $observer
+     */
     public function saveJirafeStoreEmailMapping ($observer)
     {
         $user = $observer->getEvent()->getObject();
-        $jirafeStoreIds = implode(',' ,Mage::app()->getRequest()->getPost('jirafe_send_email_for_store'));
         $jirafeEmailReportType = Mage::app()->getRequest()->getPost('jirafe_email_report_type');
-        $jirafeEmailSuppress = (int)Mage::app()->getRequest()->getPost('jirafe_email_suppress');
-        $jirafeEmails = str_replace(array("\r"," "),"",str_replace("\n",",",Mage::app()->getRequest()->getPost('jirafe_emails')));
 
-        $jirafeSettingsHaveChanged = false;
-        if($jirafeStoreIds != $user->getJirafeSendEmailForStore()){
-            $user->setJirafeSendEmailForStore($jirafeStoreIds);
-            $user->setDataChanges(true);
-            $jirafeSettingsHaveChanged = true;
-        }
-        if($jirafeEmailReportType != $user->getJirafeEmailReportType()){
-            $user->setJirafeEmailReportType($jirafeEmailReportType);
-            $user->setDataChanges(true);
-            $jirafeSettingsHaveChanged = true;
-        }
-        if($jirafeEmailSuppress != $user->getJirafeEmailSuppress()){
-            $user->setJirafeEmailSuppress($jirafeEmailSuppress);
-            $user->setDataChanges(true);
-            $jirafeSettingsHaveChanged = true;
-        }
-        if($jirafeEmails != $user->getJirafeEmails()){
-            $user->setJirafeEmails($jirafeEmails);
-            $user->setDataChanges(true);
-            $jirafeSettingsHaveChanged = true;
-        }
+        if ($jirafeEmailReportType) {
+            //we have Jirafe POST data from the My Account Form = we are updating an existing user
+            $jirafeStoreIds = implode(',', Mage::app()->getRequest()->getPost('jirafe_send_email_for_store'));
+            $jirafeEmailSuppress = (int) Mage::app()->getRequest()->getPost('jirafe_email_suppress');
+            $jirafeEmails = str_replace(array("\r", " "), "", str_replace("\n", ",", Mage::app()->getRequest()->getPost('jirafe_emails')));
 
-        if($jirafeSettingsHaveChanged) {
-            Mage::getModel('foomanjirafe/jirafe')->syncUsersAndStores();
+            $jirafeSettingsHaveChanged = false;
+            if ($jirafeStoreIds != $user->getJirafeSendEmailForStore()) {
+                $user->setJirafeSendEmailForStore($jirafeStoreIds);
+                $user->setDataChanges(true);
+                $jirafeSettingsHaveChanged = true;
+            }
+            if ($jirafeEmailReportType != $user->getJirafeEmailReportType()) {
+                $user->setJirafeEmailReportType($jirafeEmailReportType);
+                $user->setDataChanges(true);
+                $jirafeSettingsHaveChanged = true;
+            }
+            if ($jirafeEmailSuppress != $user->getJirafeEmailSuppress()) {
+                $user->setJirafeEmailSuppress($jirafeEmailSuppress);
+                $user->setDataChanges(true);
+                $jirafeSettingsHaveChanged = true;
+            }
+            if ($jirafeEmails != $user->getJirafeEmails()) {
+                $user->setJirafeEmails($jirafeEmails);
+                $user->setDataChanges(true);
+                $jirafeSettingsHaveChanged = true;
+            }
+            if($jirafeSettingsHaveChanged) {
+                Mage::getModel('foomanjirafe/jirafe')->syncUsersAndStores();
+            }
         }
     }
 
