a68dfa23a21b67d24ee4764d5c5cc3ff8c8aeced
Fooman <kristof@fooman.co.nz>
2011-04-14 21:35:31 +1200

    make sure we only have 1 upgrade to 0.2.5

--- app/code/community/Fooman/Jirafe/sql/foomanjirafe_setup/mysql4-upgrade-0.2.4-0.2.5.php
+++ app/code/community/Fooman/Jirafe/sql/foomanjirafe_setup/mysql4-upgrade-0.2.4-0.2.5.php
@@ -13,3 +13,34 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 Mage::log('Running Fooman Jirafe DB upgrade 0.2.5');
+$installer = $this;
+/* @var $installer Fooman_Jirafe_Model_Mysql4_Setup */
+
+$installer->startSetup();
+
+// Modify tables with the new DB schema
+Mage::helper('foomanjirafe/setup')->runDbSchemaUpgrade($installer, '0.2.5');
+
+// Loop through the users and tone down emailing to just those who need it
+$adminUsers = Mage::getSingleton('admin/user')->getCollection();
+foreach ($adminUsers as $adminUser) {
+	$adminUser
+		->setJirafeEmailSuppress('1')
+		->save();
+}
+
+// Once complete, reinit config files
+// reloading the config on earlier Magento versions causes an infinite loop
+if (version_compare(Mage::getVersion(), '1.3.4.0') > 0) {
+    Mage::app()->getConfig()->reinit();
+}
+
+//Make sure the default (admin) store is loaded
+$defaultStoreId = Mage_Catalog_Model_Abstract::DEFAULT_STORE_ID;
+Mage::app()->getStore($defaultStoreId)->load($defaultStoreId);
+
+// Run cron for the first time since the upgrade, so that users can see any changes right away.
+Mage::getModel('foomanjirafe/report')->cron(null, true);
+
+$installer->endSetup();
+
