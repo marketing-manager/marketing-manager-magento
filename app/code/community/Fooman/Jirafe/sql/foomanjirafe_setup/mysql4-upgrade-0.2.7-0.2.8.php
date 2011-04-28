fd89f6f46b3344b23e9aad11a1191a689ce451c7
Fooman <kristof@fooman.co.nz>
2011-04-29 09:18:28 +1200

    db upgrade file 0.2.8
    --HG--
    rename : app/code/community/Fooman/Jirafe/sql/foomanjirafe_setup/mysql4-upgrade-0.2.5-0.2.6.php => app/code/community/Fooman/Jirafe/sql/foomanjirafe_setup/mysql4-upgrade-0.2.7-0.2.8.php

---
+++ app/code/community/Fooman/Jirafe/sql/foomanjirafe_setup/mysql4-upgrade-0.2.7-0.2.8.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @package     Fooman_Jirafe
+ * @copyright   Copyright (c) 2010 Jirafe Inc (http://www.jirafe.com)
+ * @copyright   Copyright (c) 2010 Fooman Limited (http://www.fooman.co.nz)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+$version = '0.2.8';
+Mage::log('Running Fooman Jirafe DB upgrade '.$version);
+
+//Run sync when finished with install/update
+Mage::getModel('foomanjirafe/jirafe')->initialSync($version);
